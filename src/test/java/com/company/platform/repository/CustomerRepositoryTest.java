499fcf44dc89576c761adb6ab295502db71ad485
Ankit Aggarwal <17708554+iam-aki@users.noreply.github.com>
2025-07-04 18:48:01 +0530

    Changes for container are added

--- src/test/java/com/company/platform/repository/CustomerRepositoryTest.java
+++ src/test/java/com/company/platform/repository/CustomerRepositoryTest.java
@@ -4,6 +4,7 @@ import com.company.platform.model.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -12,6 +13,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Profile("testcontainer")
 @Testcontainers
 @DataMongoTest
 public class CustomerRepositoryTest {
